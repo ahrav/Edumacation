c4afa830ea14a6457f8964237b99931e7b2cb972
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-19 16:38:01 -0700

    set up registration serializer and serializer view

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import RegistrationSerializer
+
+
+class RegistrationAPIView(APIView):
+    """Allow any user to access endpoint to create/ register account"""
+
+    permission_class = (AllowAny,)
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        """Method to handle creating a new user"""
+
+        user = request.data.get("user", {})
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
