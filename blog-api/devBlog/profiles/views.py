a2d27245b5201050a4af0f24e6c6b9a1ae6ac6e6
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-22 02:34:33 -0700

    small code clean up of unused imports

--- blog-api/devBlog/profiles/views.py
+++ blog-api/devBlog/profiles/views.py
@@ -1,12 +1,13 @@
 from rest_framework import status, serializers
 from rest_framework.exceptions import NotFound
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Profile
-from .renderers import ProfileJSONRenderer
+
+# from .renderers import ProfileJSONRenderer
 from .serializers import ProfileSerializer
 
 
