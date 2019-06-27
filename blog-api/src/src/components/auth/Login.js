c5f6b59da8ca2a8e448dc6eaf4e2ed43feb5bb70
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-27 11:57:26 -0700

    add custom styling to login and register forms

--- blog-api/src/src/components/auth/Login.js
+++ blog-api/src/src/components/auth/Login.js
@@ -4,6 +4,8 @@ import { Redirect, Link, withRouter } from 'react-router-dom';
 
 import { login } from '../../actions/auth';
 import ListErrors from '../ListErrors';
+import './style.css';
+import Register from './Register';
 
 const Login = ({ login, isAuthenticated, inProgress, errors }) => {
   const [formData, setFormData] = useState({
@@ -24,57 +26,105 @@ const Login = ({ login, isAuthenticated, inProgress, errors }) => {
   if (isAuthenticated) return <Redirect to='/' />;
 
   return (
-    <div className='auth-page'>
-      <div className='container page'>
-        <div className='row'>
-          <div className='col-md-6 offset-md-3 col-xs-12'>
-            <h1 className='text-xs-center'>Sign In</h1>
-            <p className='text-xs-center'>
-              <Link to='/register'>Need an account?</Link>
-            </p>
+    <div className='body'>
+      <div className='materialContainer'>
+        <div className='box'>
+          <div className='title'>LOGIN</div>
+          <form onSubmit={e => onSubmit(e)}>
+            <div className='input'>
+              <label>
+                <input
+                  type='email'
+                  name='email'
+                  placeholder='Email'
+                  value={email}
+                  onChange={e => onChange(e)}
+                  required
+                />
+                <span className='spin' />
+              </label>
+            </div>
 
-            <ListErrors errors={errors} />
+            <div className='input'>
+              <label>
+                <input
+                  type='password'
+                  name='password'
+                  placeholder='Password'
+                  onChange={e => onChange(e)}
+                  value={password}
+                  required
+                  minLength='8'
+                />
+                <span className='spin' />
+              </label>
+            </div>
 
-            <form onSubmit={e => onSubmit(e)}>
-              <fieldset>
-                <fieldset className='form-group'>
-                  <input
-                    className='form-control form-control-lg'
-                    type='email'
-                    name='email'
-                    placeholder='Email'
-                    value={email}
-                    onChange={e => onChange(e)}
-                    required
-                  />
-                </fieldset>
+            <div className='button login'>
+              <button disabled={inProgress}>
+                <span>GO</span> <i className='fa fa-check' />
+              </button>
+            </div>
+          </form>
 
-                <fieldset className='form-group'>
-                  <input
-                    className='form-control form-control-lg'
-                    type='password'
-                    name='password'
-                    placeholder='Password'
-                    onChange={e => onChange(e)}
-                    value={password}
-                    required
-                    minLength='8'
-                  />
-                </fieldset>
-
-                <button
-                  className='btn btn-lg btn-primary pull-xs-right'
-                  type='submit'
-                  disabled={inProgress}
-                >
-                  Sign in
-                </button>
-              </fieldset>
-            </form>
-          </div>
+          <a href='' className='pass-forgot'>
+            Forgot your password?
+          </a>
         </div>
+        <Register />
       </div>
     </div>
+    // <div className='auth-page'>
+    //   <div className='container page'>
+    //     <div className='row'>
+    //       <div className='col-md-6 offset-md-3 col-xs-12'>
+    //         <h1 className='text-xs-center'>Sign In</h1>
+    //         <p className='text-xs-center'>
+    //           <Link to='/register'>Need an account?</Link>
+    //         </p>
+
+    //         <ListErrors errors={errors} />
+
+    //         <form onSubmit={e => onSubmit(e)}>
+    //           <fieldset>
+    //             <fieldset className='form-group'>
+    //               <input
+    //                 className='form-control form-control-lg'
+    //                 type='email'
+    //                 name='email'
+    //                 placeholder='Email'
+    //                 value={email}
+    //                 onChange={e => onChange(e)}
+    //                 required
+    //               />
+    //             </fieldset>
+
+    //             <fieldset className='form-group'>
+    //               <input
+    //                 className='form-control form-control-lg'
+    // type='password'
+    // name='password'
+    // placeholder='Password'
+    // onChange={e => onChange(e)}
+    // value={password}
+    // required
+    // minLength='8'
+    //               />
+    //             </fieldset>
+
+    //             <button
+    //               className='btn btn-lg btn-primary pull-xs-right'
+    //               type='submit'
+    //               disabled={inProgress}
+    //             >
+    //               Sign in
+    //             </button>
+    //           </fieldset>
+    //         </form>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- blog-api/src/src/components/auth/Register.js
+++ blog-api/src/src/components/auth/Register.js
@@ -36,81 +36,150 @@ const Register = ({
     return <Redirect to='/' />;
   }
   return (
-    <div className='auth-page'>
-      <div className='container page'>
-        <div className='row'>
-          <div className='col-md-6 offset-md-3 col-xs-12'>
-            <h1 className='text-xs-center'>Sign Up</h1>
-            <p className='text-xs-center'>
-              <Link to='/login'>Have an account?</Link>
-            </p>
+    <form onSubmit={e => onSubmit(e)}>
+      <div className='overbox'>
+        <div className='material-button alt-2'>
+          <span className='shape' />
+        </div>
 
-            <ListErrors errors={errors} />
+        <div className='title'>REGISTER</div>
 
-            <form onSubmit={e => onSubmit(e)}>
-              <fieldset>
-                <fieldset className='form-group'>
-                  <input
-                    className='form-control form-control-lg'
-                    type='text'
-                    name='username'
-                    placeholder='Username'
-                    value={username}
-                    onChange={e => onChange(e)}
-                    required
-                  />
-                </fieldset>
+        <div className='input'>
+          <label>
+            <input
+              type='text'
+              name='username'
+              placeholder='Username'
+              value={username}
+              onChange={e => onChange(e)}
+              required
+            />
+            <span className='spin' />
+          </label>
+        </div>
+        <div className='input'>
+          <label>
+            <input
+              type='email'
+              name='email'
+              placeholder='Email'
+              value={email}
+              onChange={e => onChange(e)}
+              required
+            />
+            <span className='spin' />
+          </label>
+        </div>
 
-                <fieldset className='form-group'>
-                  <input
-                    className='form-control form-control-lg'
-                    type='email'
-                    placeholder='Email'
-                    name='email'
-                    value={email}
-                    onChange={e => onChange(e)}
-                    required
-                  />
-                </fieldset>
+        <div className='input'>
+          <label />
+          <input
+            type='password'
+            name='password'
+            placeholder='Password'
+            onChange={e => onChange(e)}
+            value={password}
+            required
+            minLength='8'
+          />
+          <span className='spin' />
+        </div>
 
-                <fieldset className='form-group'>
-                  <input
-                    className='form-control form-control-lg'
-                    type='password'
-                    placeholder='Enter Password'
-                    name='password'
-                    value={password}
-                    onChange={e => onChange(e)}
-                    required
-                    minLength='8'
-                  />
-                </fieldset>
+        <div className='input'>
+          <label />
+          <input
+            type='password'
+            placeholder='Re-enter Password'
+            name='password2'
+            required
+            value={password2}
+            onChange={e => onChange(e)}
+          />
+          <span className='spin' />
+        </div>
 
-                <fieldset className='form-group'>
-                  <input
-                    className='form-control form-control-lg'
-                    type='password'
-                    placeholder='Re-enter Password'
-                    name='password2'
-                    required
-                    value={password2}
-                    onChange={e => onChange(e)}
-                  />
-                </fieldset>
-
-                <button
-                  className='btn btn-lg btn-primary pull-xs-right'
-                  type='submit'
-                  disabled={inProgress}
-                >
-                  Sign in
-                </button>
-              </fieldset>
-            </form>
-          </div>
+        <div className='button'>
+          <button>
+            <span>NEXT</span>
+          </button>
         </div>
       </div>
-    </div>
+    </form>
+    // <div className='auth-page'>
+    //   <div className='container page'>
+    //     <div className='row'>
+    //       <div className='col-md-6 offset-md-3 col-xs-12'>
+    //         <h1 className='text-xs-center'>Sign Up</h1>
+    //         <p className='text-xs-center'>
+    //           <Link to='/login'>Have an account?</Link>
+    //         </p>
+
+    //         <ListErrors errors={errors} />
+
+    //         <form onSubmit={e => onSubmit(e)}>
+    //           <fieldset>
+    //             <fieldset className='form-group'>
+    //               <input
+    //                 className='form-control form-control-lg'
+    //                 type='text'
+    //                 name='username'
+    //                 placeholder='Username'
+    //                 value={username}
+    //                 onChange={e => onChange(e)}
+    //                 required
+    //               />
+    //             </fieldset>
+
+    //             <fieldset className='form-group'>
+    //               <input
+    //                 className='form-control form-control-lg'
+    //                 type='email'
+    //                 placeholder='Email'
+    //                 name='email'
+    //                 value={email}
+    //                 onChange={e => onChange(e)}
+    //                 required
+    //               />
+    //             </fieldset>
+
+    //             <fieldset className='form-group'>
+    //               <input
+    //                 className='form-control form-control-lg'
+    //                 type='password'
+    //                 placeholder='Enter Password'
+    //                 name='password'
+    //                 value={password}
+    //                 onChange={e => onChange(e)}
+    //                 required
+    //                 minLength='8'
+    //               />
+    //             </fieldset>
+
+    //             <fieldset className='form-group'>
+    //               <input
+    //                 className='form-control form-control-lg'
+    //                 type='password'
+    //                 placeholder='Re-enter Password'
+    //                 name='password2'
+    //                 required
+    //                 value={password2}
+    //                 onChange={e => onChange(e)}
+    //               />
+    //             </fieldset>
+
+    //             <button
+    //               className='btn btn-lg btn-primary pull-xs-right'
+    //               type='submit'
+    //               disabled={inProgress}
+    //             >
+    //               Sign in
+    //             </button>
+    //           </fieldset>
+    //         </form>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
