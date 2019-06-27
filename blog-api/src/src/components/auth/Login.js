bdfa11c22ca49ef00ee0f06bdd2140a45edaa354
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-27 11:58:08 -0700

    remove comments from login and register

--- blog-api/src/src/components/auth/Login.js
+++ blog-api/src/src/components/auth/Login.js
@@ -74,57 +74,6 @@ const Login = ({ login, isAuthenticated, inProgress, errors }) => {
         <Register />
       </div>
     </div>
-    // <div className='auth-page'>
-    //   <div className='container page'>
-    //     <div className='row'>
-    //       <div className='col-md-6 offset-md-3 col-xs-12'>
-    //         <h1 className='text-xs-center'>Sign In</h1>
-    //         <p className='text-xs-center'>
-    //           <Link to='/register'>Need an account?</Link>
-    //         </p>
-
-    //         <ListErrors errors={errors} />
-
-    //         <form onSubmit={e => onSubmit(e)}>
-    //           <fieldset>
-    //             <fieldset className='form-group'>
-    //               <input
-    //                 className='form-control form-control-lg'
-    //                 type='email'
-    //                 name='email'
-    //                 placeholder='Email'
-    //                 value={email}
-    //                 onChange={e => onChange(e)}
-    //                 required
-    //               />
-    //             </fieldset>
-
-    //             <fieldset className='form-group'>
-    //               <input
-    //                 className='form-control form-control-lg'
-    // type='password'
-    // name='password'
-    // placeholder='Password'
-    // onChange={e => onChange(e)}
-    // value={password}
-    // required
-    // minLength='8'
-    //               />
-    //             </fieldset>
-
-    //             <button
-    //               className='btn btn-lg btn-primary pull-xs-right'
-    //               type='submit'
-    //               disabled={inProgress}
-    //             >
-    //               Sign in
-    //             </button>
-    //           </fieldset>
-    //         </form>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
