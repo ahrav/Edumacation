ac9be38a63c23c6f265481f5cc1e0d7da1f0be23
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-30 12:58:48 -0700

    refactor login form

--- blog-api/src/src/components/auth/Login.js
+++ blog-api/src/src/components/auth/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
@@ -16,29 +16,16 @@ const initialFormValues = {
   password: ''
 };
 
-const Login = ({ login, isAuthenticated, loading, errors }) => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: ''
-  });
-
-  const { email, password } = formData;
-
-  const onChange = e =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  const onSubmit = async e => {
-    e.preventDefault();
-    login(email, password);
-    setFormData({ email: '', password: '' });
-  };
-
+const Login = ({ login, isAuthenticated }) => {
   if (isAuthenticated) return <Redirect to='/' />;
 
   return (
     <div className='limiter'>
       <div className='container-login100'>
-        <div className='wrap-login100'>
+        <div
+          className='wrap-login100'
+          style={{ padding: '175px 90px 125px 75px' }}
+        >
           <Tilt
             options={{ max: 55, perspective: 300, scale: 1.1, speed: 500 }}
             className='login100-pic js-tilt'
@@ -60,27 +47,18 @@ const Login = ({ login, isAuthenticated, loading, errors }) => {
               login(email, password);
             }}
           >
-            {({
-              email,
-              password,
-              handleBlur,
-              error,
-              touched,
-              isSubmitting,
-              setFieldValue
-            }) => (
+            {({ isSubmitting }) => (
               <Form className='login100-form validate-form'>
                 <span className='login100-form-title'>Login</span>
                 <Field
                   name='email'
-                  type='email'
-                  placeholder='Email'
                   render={({ field, form: { isSubmitting } }) => (
                     <Fragment>
                       <div className='wrap-input100 validate-input'>
                         <input
                           {...field}
                           className='input100'
+                          type='email'
                           placeholder='Email'
                           disabled={isSubmitting}
                         />
@@ -91,83 +69,62 @@ const Login = ({ login, isAuthenticated, loading, errors }) => {
                         </span>
                       </div>
                       <ErrorMessage name='email'>
-                        {msg => <span className='alert-danger'>{msg}</span>}
+                        {msg => <div className='alert-danger'>{msg}</div>}
                       </ErrorMessage>
                     </Fragment>
                   )}
                 />
-              </Form>
-            )}
-            {/* <form
-            onSubmit={e => onSubmit(e)}
-            className='login100-form validate-form'
-          >
-            <span className='login100-form-title'>Login</span>
-            <div
-              className='wrap-input100 validate-input'
-            ></div>
+                <Field
+                  name='password'
+                  render={({ field, form: { isSubmitting } }) => (
+                    <Fragment>
+                      <div className='wrap-input100 validate-input'>
+                        <input
+                          {...field}
+                          className='input100'
+                          placeholder='Password'
+                          type='password'
+                          disabled={isSubmitting}
+                        />
 
-            <div
-              className='wrap-input100 validate-input'
-              data-validate='Valid email is required: ex@abc.xyz'
-            >
-              <input
-                className='input100'
-                type='email'
-                required
-                value={email}
-                onChange={e => onChange(e)}
-                name='email'
-                placeholder='Email'
-              />
-              <span className='focus-input100' />
-              <span className='symbol-input100'>
-                <i className='fa fa-envelope' aria-hidden='true' />
-              </span>
-            </div>
+                        <span className='focus-input100' />
+                        <span className='symbol-input100'>
+                          <i className='fa fa-lock' aria-hidden='true' />
+                        </span>
+                      </div>
+                      <ErrorMessage name='password'>
+                        {msg => <div className='alert-danger'>{msg}</div>}
+                      </ErrorMessage>
+                    </Fragment>
+                  )}
+                />
+                <div className='container-login100-form-btn'>
+                  <button
+                    disabled={isSubmitting}
+                    className='login100-form-btn'
+                  >
+                    Login
+                  </button>
+                </div>
 
-            <div
-              className='wrap-input100 validate-input'
-              data-validate='Password is required'
-            >
-              <input
-                className='input100'
-                type='password'
-                name='password'
-                placeholder='Password'
-                onChange={e => onChange(e)}
-                value={password}
-                required
-              />
-              <span className='focus-input100' />
-              <span className='symbol-input100'>
-                <i className='fa fa-lock' aria-hidden='true' />
-              </span>
-            </div>
-
-            <div className='container-login100-form-btn'>
-              <button disabled={loading} className='login100-form-btn'>
-                Login
-              </button>
-            </div>
-
-            <div className='text-center p-t-12'>
+                {/* <div className='text-center p-t-12'>
               <span className='txt1'>Forgot </span>
               <a className='txt2' href='#'>
                 Username / Password?
               </a>
-            </div>
+            </div> */}
 
-            <div className='text-center p-t-136'>
-              <Link className='txt2 create' to='/register'>
-                Create An Account
-                <i
-                  className='fa fa-long-arrow-right m-l-5'
-                  aria-hidden='true'
-                />
-              </Link>
-            </div>
-          </form> */}
+                <div className='text-center p-t-136'>
+                  <Link className='txt2 create' to='/register'>
+                    Create An Account
+                    <i
+                      className='fa fa-long-arrow-right m-l-5'
+                      aria-hidden='true'
+                    />
+                  </Link>
+                </div>
+              </Form>
+            )}
           </Formik>
         </div>
       </div>
@@ -176,9 +133,7 @@ const Login = ({ login, isAuthenticated, loading, errors }) => {
 };
 
 const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated,
-  loading: state.auth.loading,
-  errors: state.auth.errors
+  isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(
